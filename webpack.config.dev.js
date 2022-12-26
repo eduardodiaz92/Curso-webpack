6788adc900d3e8df156019a9bdf0fca64fe8e150
Eduardo Díaz <eduaedodiaz@gmail.com>
2022-12-25 18:00:22 -0600

    deploy test

---
+++ webpack.config.dev.js
@@ -0,0 +1,96 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  // Entry nos permite decir el punto de entrada de nuestra aplicación
+  entry: "./src/index.js",
+  // Output nos permite decir hacia dónde va enviar lo que va a preparar webpacks
+  output: {
+    // path es donde estará la carpeta donde se guardará los archivos
+    // Con path.resolve podemos decir dónde va estar la carpeta y la ubicación del mismo
+    path: path.resolve(__dirname, "dist"),
+    // filename le pone el nombre al archivo final
+    filename: "[name].[contenthash].js",
+    assetModuleFilename: "assets/images/[hash][ext][query]",
+  },
+  mode: "development",
+  watch: true,
+  resolve: {
+    // Aqui ponemos las extensiones que tendremos en nuestro proyecto para webpack los lea
+    extensions: [".js"],
+    alias: {
+      "@utils": path.resolve(__dirname, "src/utils/"),
+      "@templates": path.resolve(__dirname, "src/templates/"),
+      "@styles": path.resolve(__dirname, "src/styles/"),
+      "@images": path.resolve(__dirname, "src/assets/images/"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        // Lo que la expresión regular significa:
+        //Obtener todos los archivos .mjs o .js del proyecto.
+        test: /\.m?js$/,
+        // Exclude permite omitir archivos o carpetas especificas
+        exclude: /node_modules/,
+        // Use es un arreglo u objeto donde dices que loader aplicaras
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        //Obtener los archivos .css y .styl del proyecto.
+        test: /\.css|.styl$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"],
+      },
+      {
+        test: /\.png/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.woff|woff2$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 1000,
+            mimetype: "application/font-woff",
+            name: "[name].[contenthash].[ext]",
+            outputPath: "./assets/fonts/",
+            publicPath: "./assets/fonts/",
+            esModule: false,
+          },
+        },
+      },
+    ],
+  },
+  // SECCION DE PLUGINS
+  plugins: [
+    new HtmlWebpackPlugin({
+      //CONFIGURACIÓN DEL PLUGIN
+      inject: true,
+      // INYECTA EL BUNDLE AL TEMPLATE HTML
+      template: "./public/index.html",
+      // NOMBRE FINAL DEL ARCHIVO
+      filename: "./index.html",
+      // NOMBRE FINAL DEL ARCHIVO
+    }),
+    // INSTANCIAMOS EL PLUGIN
+    new MiniCssExtractPlugin({
+      filename: "assets/[name].[contenthash].css",
+    }),
+    //From ⇒ que recurso (archivo o directorio) deseamos copiar al directorio final
+    //To ⇒ en que ruta dentro de la carpeta final terminara los recursos
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images",
+        },
+      ],
+    }),
+    new Dotenv(),
+  ],
+};
